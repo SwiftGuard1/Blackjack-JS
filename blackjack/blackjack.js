4908a5161449ec3ed0cdd38eb8de17f3d51660cc
SwiftGuard1 <79541415+SwiftGuard1@users.noreply.github.com>
2023-02-06 12:56:27 -0500

    Update blackjack.js

--- blackjack/blackjack.js
+++ blackjack/blackjack.js
@@ -39,11 +39,11 @@ const dealCard = () => {
 }
 
 const playGame = () => {
-  console.log('Welcome to blackjack!');
+  alert('Welcome to blackjack!');
   firstCards = initialDeal();
   currentScore += firstCards[card1].val + firstCards[card2].val;
-  console.log(`Your first cards are: ${firstCards[card1]} and ${firstCards[card2]});
-  console.log(`Your score is ${currentScore});
+  alert(`Your first cards are: ${firstCards[card1]} and ${firstCards[card2]}`);
+  alert(`Your score is ${currentScore}`);
   dealCard();
 }
 
